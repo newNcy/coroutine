9b9e9ab5bbb29227b59fc3fcb74876ba967792ac
ncy <1136962813@qq.com>
2021-11-28 06:31:31 +0800

    hook个sleep看看

--- co.c
+++ co.c
@@ -1,21 +1,90 @@
 #include <stdio.h>
 #include "coroutine.h"
+#include <time.h>
 
-void foo(coroutine_t* co)
+#include <malloc.h>
+
+typedef struct co_timer_t 
 {
-    printf("foo 1\n");
-    co_yield(co);
-    printf("foo 2\n");
+    int co_id;
+    time_t expiration_time;
+    struct co_timer_t * next;
+}co_timer_t;
+
+typedef struct co_timer_mgr_t
+{
+    co_timer_t * head;
+    co_timer_t * tail;
+}co_timer_mgr_t;
+
+co_timer_mgr_t co_timer_mgr = {0};
+
+
+void sleep(int s)
+{
+    if (s <= 0) {
+        return;
+    }
+    co_timer_t * timer = (co_timer_t*)malloc(sizeof(co_timer_t));
+    timer->co_id = co_running();
+    timer->expiration_time = time(0) + s;
+    timer->next = NULL;
+
+    if (!co_timer_mgr.head) {
+        co_timer_mgr.tail = co_timer_mgr.head = timer;
+    } else {
+        co_timer_mgr.tail->next = timer;
+        co_timer_mgr.tail = timer;
+    }
+    co_yield();
 }
+
+void foo()
+{
+    for (int i = 0; i < 3; ++ i) {
+        sleep(1);
+        printf("sleep 1s\n");
+        fflush(stdout);
+    }
+}
+
+int has_timer()
+{
+    return co_timer_mgr.head != NULL;
+}
+
+void process_timer()
+{
+    co_timer_t * cur = co_timer_mgr.head;
+    time_t now = time(0);
+    while (cur) {
+        if (cur->expiration_time <= now) {
+            co_resume(cur->co_id);
+            co_timer_t * next = cur->next;
+            free(cur);
+            cur = next;
+        } else {
+            break;
+        }
+    }
+    co_timer_mgr.head = cur;
+    if (!cur) {
+        co_timer_mgr.tail = NULL;
+    }
+}
+
 
 int main()
 {
-    coroutine_t * co = co_create((coroutine_entry_t)foo);
-    printf("main 1\n");
+
+    co_init(10);
+
+    int co = co_create((coroutine_entry_t)foo);
     co_resume(co);
-    printf("main 2\n");
-    co_resume(co);
-    printf("main 3\n");
-    co_destroy(co);
+
+    while (has_timer()) {
+        process_timer();
+    }
+    co_finish();
     return 0;
 }
--- coroutine.h
+++ coroutine.h
@@ -4,26 +4,6 @@
 #include <stdint.h>
 #define CO_STACK_SIZE 1024 * 100
 
-typedef uint64_t reg_t;
-
-typedef struct 
-{
-    reg_t rsp;
-    reg_t rbp;
-    reg_t rip;
-
-    reg_t rcx;
-    reg_t rdx;
-
-    //callee save
-    reg_t rbx;
-    reg_t r12;
-    reg_t r13;
-    reg_t r14;
-    reg_t r15;
-} context_t;
-
-
 typedef enum 
 {
     CO_SUSPEND,
@@ -31,22 +11,15 @@ typedef enum
     CO_FINISH
 }co_status_t;
 
-struct coroutine_t;
-typedef void * (*coroutine_entry_t)(struct coroutine_t *,void *args);
+static int CO_ID_INVALID = -1;
 
-typedef struct coroutine_t
-{
-    context_t ctx;
-    context_t main;
-    coroutine_entry_t entry;
-    co_status_t status;
+typedef void * (*coroutine_entry_t)();
 
-    char * stack;
-}coroutine_t;
-
-coroutine_t * co_create(coroutine_entry_t entry);
-void co_resume(coroutine_t * co);
-void co_yield(coroutine_t * co);
-void co_destroy(coroutine_t *co);
+void co_init(int max);
+int co_create(coroutine_entry_t entry);
+void co_resume(int co);
+void co_yield();
+int co_running();
+void co_finish();
 
 
